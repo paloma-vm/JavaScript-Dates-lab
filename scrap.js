986fc5ba2fb6ce9dc9e4063b75aa7c4daa0b75b2
paloma-vm <paloma.valdez-marsh@students.dominican.edu>
2023-09-09 12:13:13 -0700

    finished problem 2

---
+++ scrap.js
@@ -0,0 +1,8 @@
+const stringlib = require('@palomavm/string-lib')
+console.log('**********************')
+console.log(stringlib.capitalize('hello'))
+console.log('**********************')
+console.log(stringlib.capitalizeWords('this is fine'))
+
+const today = new Date() // gets the time now
+console.log(today)
